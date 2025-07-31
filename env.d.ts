c6dc7bef1d7d1b4b1da9024a7057041cc8160c0d
DevPanda (F. Richter) <info@florianrichter.eu>
2025-07-31 10:32:09 +0200

    env types

--- env.d.ts
+++ env.d.ts
@@ -3,5 +3,9 @@ declare module "bun" {
     DATABASE_URL: string;
     BETTER_AUTH_SECRET: string;
     BETTER_AUTH_URL: string;
+    GITHUB_CLIENT_ID: string;
+    GITHUB_CLIENT_SECRET: string;
+    DISCORD_CLIENT_ID: string;
+    DISCORD_CLIENT_SECRET: string;
   }
 }
