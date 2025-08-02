22eab187e67165e0582f6f4c060238820c51a207
DevPanda (F. Richter) <info@florianrichter.eu>
2025-08-02 21:20:26 +0200

    trying to add orpc

--- src/routes/_app/users.tsx
+++ src/routes/_app/users.tsx
@@ -1,19 +1,19 @@
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
-
-import { useTRPC } from "@/trpc/react";
+import {orpc} from "@/orpc/client.ts";
 
 export const Route = createFileRoute("/_app/users")({
   loader: async ({ context }) =>
     await context.queryClient.ensureQueryData(
-      context.trpc.user.getAll.queryOptions(),
+      orpc.test.queryOptions({
+        retry: true
+      })
     ),
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  const trpc = useTRPC();
-  const { data } = useSuspenseQuery(trpc.user.getAll.queryOptions());
+  const { data } = useSuspenseQuery(orpc.test.queryOptions());
 
   return (
     <article className="space-y-4">
@@ -31,17 +31,7 @@ function RouteComponent() {
             </tr>
           </thead>
           <tbody className="divide-y divide-slate-300 border-y border-slate-300 align-middle">
-            {data.map(({ id, email, image, name }) => (
-              <tr key={id} className="align-middle">
-                <td className="px-4 py-2 align-middle whitespace-nowrap">
-                  {image ? <img src={image} alt={email} /> : null}
-                  {email}
-                </td>
-                <td className="px-4 py-2 align-middle whitespace-nowrap">
-                  {name}
-                </td>
-              </tr>
-            ))}
+            {data}
           </tbody>
         </table>
       </section>
